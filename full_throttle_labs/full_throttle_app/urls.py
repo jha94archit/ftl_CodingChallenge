da85e0c4461ba04201df481814ea16c7eb789829
architjha <jha94archit@gmail.com>
2020-07-03 11:43:53 +0530

    Required output achieved

--- full_throttle_labs/full_throttle_app/urls.py
+++ full_throttle_labs/full_throttle_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'full_throttle_app'
 
 urlpatterns = [
-    path('api/member_activity/', views.MemberActivity.as_view(), name='member_activity'),
+    path('api/member/', views.MemberActivity.as_view(), name='member'),
     path('api/activity/', views.ActivityView.as_view(), name='activity'),
+    path('api/member_activity/', views.MemberActivityView.as_view(), name='member_activity'),
 ]
--- full_throttle_labs/full_throttle_app/views.py
+++ full_throttle_labs/full_throttle_app/views.py
@@ -1,6 +1,29 @@
 from core.models import Member, ActivityPeriods
+from rest_framework.response import Response
 from .serializers import *
+from rest_framework.views import APIView
 from rest_framework import generics
+import json
+
+
+
+class MemberActivityView(APIView):
+
+    def get(self, request):
+        queryset = Member.objects.all()
+        if queryset:
+            serializer_class = MemberSerializer(queryset, many=True)
+            member_activity_data = serializer_class.data
+            content = {
+                "ok": True,
+                "members": member_activity_data
+            }
+            json.dumps(content)
+            return Response(content)
+        else:
+            return Response({
+                "ok": False
+            })
 
 
 class MemberActivity(generics.ListAPIView):
